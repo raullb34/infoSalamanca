3415efc6abf72ec61891598fbee5125670a3ab74
Raúl López <raullb@usal.es>
2025-08-02 16:35:01 +0200

    docker compose errors fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const HOST = process.env.BACKEND_HOST || 'localhost';
+const HOST = process.env.BACKEND_HOST || '0.0.0.0';
 const PORT = process.env.BACKEND_PORT || 4000;
 
 const app = express();
